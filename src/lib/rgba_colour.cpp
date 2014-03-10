f73a92c024bb20be05501b59c3a28ddea2c96f30
abainbridge2005@gmail.com <abainbridge2005@gmail.com@d4f9cf96-75be-aa48-c31c-9549f9317f33>
2014-03-10 21:21:41 +0000

    Fixed the definitions of g_colourWhite and g_colourBlack which were missing a hex digit. This bug was breaking the harmonograph example now that PutPix pays attention to alpha values.

--- src/lib/rgba_colour.cpp
+++ src/lib/rgba_colour.cpp
@@ -1,5 +1,5 @@
 #include "rgba_colour.h"
 
 
-RGBAColour g_colourBlack = { 0x0000000U };
-RGBAColour g_colourWhite = { 0xfffffffU };
+RGBAColour g_colourBlack = { 0xff000000U };
+RGBAColour g_colourWhite = { 0xffffffffU };
