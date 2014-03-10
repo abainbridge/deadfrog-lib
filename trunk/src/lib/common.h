97b8d757144c4fb4dcc082544a1760b53c08e25f
abainbridge2005@gmail.com <abainbridge2005@gmail.com@d4f9cf96-75be-aa48-c31c-9549f9317f33>
2014-03-10 21:17:12 +0000

    Tidied up the project files and DLL_API macro so that all the examples are statically linked and everything uses the non-DLL VC runtime. This way any exes built are truly portable.

--- trunk/src/lib/common.h
+++ trunk/src/lib/common.h
@@ -3,29 +3,30 @@
 
 #ifdef DLL_EXPORTS
  #define DLL_API extern "C" __declspec(dllexport)
-#elif STATIC_LIB
- #define DLL_API extern
-#else
+#elif DLL_IMPORTS
  #define DLL_API extern "C" __declspec(dllimport)
+#else
+ #define DLL_API extern
 #endif
 
 
 void DebugOut(char *fmt, ...);
 
-
 void ReleaseAssert(bool condition, char const *_fmt, ...);
 void ReleaseWarn(bool condition, char const *_fmt, ...);
 
+// Define an assert macro that stops in the debugger in Debug builds but does 
+// nothing in release builds
 #ifdef _DEBUG
-#ifdef _MSC_VER
-#include <crtdbg.h>
-#define DebugAssert(x) if(!(x)) { _CrtDbgBreak(); }
+ #ifdef _MSC_VER
+  #include <crtdbg.h>
+  #define DebugAssert(x) if(!(x)) { _CrtDbgBreak(); }
+ #else
+  #include <assert.h>
+  #define DebugAssert(x) assert(x)
+ #endif
 #else
-#include <assert.h>
-#define DebugAssert(x) assert(x)
-#endif
-#else
-#define DebugAssert(x)
+ #define DebugAssert(x)
 #endif
 
 
