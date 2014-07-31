03d76b3d7197d7511220ca5c75204860a4ec70a7
abainbridge2005@gmail.com <abainbridge2005@gmail.com@d4f9cf96-75be-aa48-c31c-9549f9317f33>
2014-07-31 20:00:46 +0000

    Making the polygon code look more like the style of everything else

--- trunk/src/lib/polygon.h
+++ trunk/src/lib/polygon.h
@@ -4,21 +4,26 @@
 #include "bitmap.h"
 
 // Describes a single point (used for a single vertex)
-typedef struct {
-   int X;
-   int Y;
+typedef struct 
+{
+    int X;
+    int Y;
 } Point;
+
 
 // Describes a series of points (used to store a list of vertices that
 // describe a polygon; each vertex is assumed to connect to the two
 // adjacent vertices, and the last vertex is assumed to connect to the
 // first)
-typedef struct {
-   int Length;          // # of points
-   Point *PointPtr;     // pointer to list of points
+typedef struct 
+{
+    int Length;          // # of points
+    Point *PointPtr;     // pointer to list of points
 } PointListHeader;
 
-int FillConvexPolygon(BitmapRGBA *bmp, PointListHeader * VertexList, RGBAColour Color,
-                      int XOffset, int YOffset);
+
+int FillConvexPolygon(BitmapRGBA *bmp, PointListHeader *vertexList, RGBAColour col,
+                      int xOffset, int yOffset);
+
 
 #endif
