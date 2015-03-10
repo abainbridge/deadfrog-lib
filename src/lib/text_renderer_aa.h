29fa97243188b78716873d4db7333ca58350da9d
abainbridge2005@gmail.com <abainbridge2005@gmail.com@d4f9cf96-75be-aa48-c31c-9549f9317f33>
2015-03-10 21:23:13 +0000

    Added first pass at an antialiased version of the text renderer. It is not production quality yet. The kerning is awful and I haven't implemented clipped rendering. There may be other problems too.

---
+++ src/lib/text_renderer_aa.h
@@ -0,0 +1,37 @@
+// This module implements an antialiased text renderer.
+// You create a font by specifying the name of any installed font and
+// the point size you want. The font's glyphs will be pre-calculated.
+// Variable width fonts are supported.
+
+#ifndef TEXT_RENDERER_AA_H
+#define TEXT_RENDERER_AA_H
+
+
+#include "lib/rgba_colour.h"
+#include "lib/common.h"
+
+struct BitmapRGBA;
+class GlyphAa;
+
+
+typedef struct
+{
+	GlyphAa     *glyphs[256];
+	bool		fixedWidth;
+	int			maxCharWidth;
+	int			charHeight;	// in pixels
+} TextRendererAa;
+
+
+// Size is in pixels. Weight is in range 1 (thin) to 9 (heavy)
+DLL_API TextRendererAa *CreateTextRendererAa(char const *fontname, int size, int weight);
+
+DLL_API int DrawTextSimple		(TextRendererAa *, RGBAColour c, BitmapRGBA *, int x, int y, char const *text);       // Returns text length in pixels
+DLL_API int DrawTextLeft		(TextRendererAa *, RGBAColour c, BitmapRGBA *, int x, int y, char const *text, ...);	// Like simple but with variable args
+DLL_API int DrawTextRight		(TextRendererAa *, RGBAColour c, BitmapRGBA *, int x, int y, char const *text, ...);	// Like above but with right justify
+DLL_API int DrawTextCentre		(TextRendererAa *, RGBAColour c, BitmapRGBA *, int x, int y, char const *text, ...);	// Like above but with centre justify
+
+DLL_API int	GetTextWidth		(TextRendererAa *, char const *text, int len=999999);
+
+
+#endif
