bd333655166ee3082e09d0b2bbb0c9df76942be8
Andrew Bainbridge <abainbridge@users.noreply.github.com>
2020-06-06 13:23:10 +0100

    Make the df_time module work on Linux.

--- src/df_time.cpp
+++ src/df_time.cpp
@@ -1,14 +1,27 @@
 #include "df_time.h"
 
+
+static double g_timeShift = 0.0;
+
+
+#if _MSC_VER
+
+
+// Windows specific code ******************************************************
+
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
 
 
 static double g_tickInterval = -1.0;
-static double g_timeShift = 0.0;
 
 
-inline double GetLowLevelTime();
+inline double GetLowLevelTime()
+{
+    LARGE_INTEGER count;
+    QueryPerformanceCounter(&count);
+    return (double)count.QuadPart * g_tickInterval;
+}
 
 
 static void InitialiseHighResTime()
@@ -20,26 +33,55 @@ static void InitialiseHighResTime()
 }
 
 
-inline double GetLowLevelTime()
+void SleepMillisec(int milliseconds)
 {
-    if (g_tickInterval < 0.0)
-        InitialiseHighResTime();
-
-    LARGE_INTEGER count;
-    QueryPerformanceCounter(&count);
-    return (double)count.QuadPart * g_tickInterval;
+	Sleep(milliseconds);
 }
 
 
-NOINLINE double GetRealTime()
+#else
+
+
+// Linux specific code ******************************************************
+
+// POSIX headers
+#include <stdint.h>
+#include <sys/time.h>
+#include <unistd.h>
+
+
+static double GetLowLevelTime()
 {
-	double timeNow = GetLowLevelTime();
-    timeNow -= g_timeShift;
-    return timeNow;
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return (double)((int64_t)tv.tv_sec * 1000000 + tv.tv_usec) / 1000000.;
+}
+
+
+static void InitialiseHighResTime()
+{
+	g_timeShift = GetLowLevelTime();
 }
 
 
 void SleepMillisec(int milliseconds)
 {
-	Sleep(milliseconds);
+	usleep(milliseconds * 1000);
+}
+
+
+#endif
+
+
+// Common code ******************************************************
+
+NOINLINE double GetRealTime()
+{
+    if (g_timeShift <= 0.0) {
+        InitialiseHighResTime();
+    }
+
+    double timeNow = GetLowLevelTime();
+    timeNow -= g_timeShift;
+    return timeNow;
 }
