4d9a7da42715c5cf6c0cfec60c38a362c227b320
Andrew Bainbridge <abainbridge@users.noreply.github.com>
2020-08-05 18:28:58 +0100

    Re-order KEY_ #defines to into numerical order. They were in no particular order before. This was done to make the up-coming X11 keysym to keycode conversion function easier to follow.

--- src/df_window.h
+++ src/df_window.h
@@ -106,6 +106,41 @@ DLL_API bool	    InputPoll();  // Returns true if any events occurred since last
 
 
 // Defines for indexes into g_input.keys[], keyDowns[] and keyUps[].
+#define KEY_BACKSPACE         8 // TODO - Convert #defines to enums.
+#define KEY_TAB               9
+#define KEY_ENTER             13
+
+#define KEY_SHIFT			  16
+#define KEY_CONTROL			  17
+#define KEY_ALT               18
+#define KEY_PAUSE             19
+#define KEY_CAPSLOCK          20
+#define KEY_ESC               27
+
+#define KEY_SPACE             32
+
+#define KEY_PGUP              33
+#define KEY_PGDN              34
+#define KEY_END               35
+#define KEY_HOME              36
+#define KEY_LEFT              37
+#define KEY_RIGHT             39
+#define KEY_UP                38
+#define KEY_DOWN              40
+#define KEY_INSERT            45
+#define KEY_DEL               46
+
+#define KEY_0                 48
+#define KEY_1                 49
+#define KEY_2                 50
+#define KEY_3                 51
+#define KEY_4                 52
+#define KEY_5                 53
+#define KEY_6                 54
+#define KEY_7                 55
+#define KEY_8                 56
+#define KEY_9                 57
+
 #define KEY_A                 65
 #define KEY_B                 66
 #define KEY_C                 67
@@ -132,17 +167,9 @@ DLL_API bool	    InputPoll();  // Returns true if any events occurred since last
 #define KEY_X                 88
 #define KEY_Y                 89
 #define KEY_Z                 90
-#define KEY_0                 48
-#define KEY_1                 49
-#define KEY_2                 50
-#define KEY_3                 51
-#define KEY_4                 52
-#define KEY_5                 53
-#define KEY_6                 54
-#define KEY_7                 55
-#define KEY_8                 56
-#define KEY_9                 57
+
 #define KEY_MENU              93
+
 #define KEY_0_PAD             96
 #define KEY_1_PAD             97
 #define KEY_2_PAD             98
@@ -153,6 +180,13 @@ DLL_API bool	    InputPoll();  // Returns true if any events occurred since last
 #define KEY_7_PAD             103
 #define KEY_8_PAD             104
 #define KEY_9_PAD             105
+
+#define KEY_ASTERISK          106
+#define KEY_PLUS_PAD          107
+#define KEY_MINUS_PAD         109
+#define KEY_DEL_PAD           110
+#define KEY_SLASH_PAD         111
+
 #define KEY_F1                112
 #define KEY_F2                113
 #define KEY_F3                114
@@ -165,45 +199,24 @@ DLL_API bool	    InputPoll();  // Returns true if any events occurred since last
 #define KEY_F10               121
 #define KEY_F11               122
 #define KEY_F12               123
-#define KEY_ESC               27
-#define KEY_BACK_TICK         223
-#define KEY_TILDE             222
-#define KEY_MINUS             189
-#define KEY_EQUALS            187
-#define KEY_BACKSPACE         8
-#define KEY_TAB               9
-#define KEY_OPENBRACE         219
-#define KEY_CLOSEBRACE        221
-#define KEY_ENTER             13
+
+#define KEY_NUMLOCK           144
+#define KEY_SCRLOCK           145
+
 #define KEY_COLON             186
-#define KEY_QUOTE             192
-#define KEY_BACKSLASH         220
+#define KEY_EQUALS            187
 #define KEY_COMMA             188
+#define KEY_MINUS             189
 #define KEY_STOP              190
 #define KEY_SLASH             191
-#define KEY_SPACE             32
-#define KEY_INSERT            45
-#define KEY_DEL               46
-#define KEY_HOME              36
-#define KEY_END               35
-#define KEY_PGUP              33
-#define KEY_PGDN              34
-#define KEY_LEFT              37
-#define KEY_RIGHT             39
-#define KEY_UP                38
-#define KEY_DOWN              40
-#define KEY_SLASH_PAD         111
-#define KEY_ASTERISK          106
-#define KEY_MINUS_PAD         109
-#define KEY_PLUS_PAD          107
-#define KEY_DEL_PAD           110
-#define KEY_PAUSE             19
-#define KEY_ALT               18
-#define KEY_SCRLOCK           145
-#define KEY_NUMLOCK           144  
-#define KEY_CAPSLOCK          20
-#define KEY_SHIFT			  16
-#define KEY_CONTROL			  17
+#define KEY_QUOTE             192
+
+#define KEY_OPENBRACE         219
+#define KEY_BACKSLASH         220
+#define KEY_CLOSEBRACE        221
+
+#define KEY_TILDE             222
+#define KEY_BACK_TICK         223
 
 
 #ifdef __cplusplus
