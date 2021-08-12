8fd67035cebad997b676c5c17ba624a6311bbefd
Andrew Bainbridge <abainbridge@users.noreply.github.com>
2021-08-12 19:47:24 +0100

    Added to ignore list.

--- examples/all.cpp
+++ examples/all.cpp
@@ -1,26 +1,37 @@
 void AaPolyTestMain();
 void BenchmarkMain();
 void ColourWhirlMain();
+void FluidMain();
 void FractalFernMain();
 void GasMain();
 void HarmonographMain();
 void HelloWorldMain();
 void MandelbrotMain();
+void PlanimeterMain();
+void SevenGuisMain();
 void Sierpinski3DMain();
 void TestMain();
 
 
+#ifdef _MSC_VER
+#include <windows.h>
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+#else
+int main()
+#endif
 {
-    AaPolyTestMain();
+//     AaPolyTestMain();
 //     BenchmarkMain();
 //     ColourWhirlMain();
+//     FluidMain();
 //     FractalFernMain();
 //     GasMain();
 //     HarmonographMain();
 //     HelloWorldMain();
 //     MandelbrotMain();
-//     Sierpinski3DMain();
+//     PlanimeterMain();
+     SevenGuisMain();
+//    Sierpinski3DMain();
 //     TestMain();
 
     return 0;
