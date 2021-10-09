0d12bca497a37aa388691e6d0faf6121d720a27c
Andrew Bainbridge <abainbridge@users.noreply.github.com>
2021-10-09 22:03:09 +0100

    Added a benchmark mode to the stretch blit example.

--- examples/stretch_blit.cpp
+++ examples/stretch_blit.cpp
@@ -1,7 +1,9 @@
 // Deadfrog includes
 #include "df_bmp.h"
+#include "df_font.h"
 #include "df_time.h"
 #include "df_window.h"
+#include "fonts/df_mono.h"
 
 // Standard includes
 #include <math.h>
@@ -10,14 +12,19 @@
 void StretchBlitMain() {
     CreateWin(1024, 768, WT_WINDOWED, "Stretch Blit Example");
 
+    g_defaultFont = LoadFontFromMemory(deadfrog_mono_7x13, sizeof(deadfrog_mono_7x13));
+
     DfBitmap *dogBmp = LoadBmp("marlieses_dog.bmp");
     if (!dogBmp) {
         dogBmp = LoadBmp("../../marlieses_dog.bmp");
     }
     ReleaseAssert(dogBmp, "Couldn't load marlieses_dog.bmp");
 
-    while (!g_window->windowClosed && !g_input.keys[KEY_ESC]) {
+    // Animate the stretch blit.
+    while (!g_input.keyDowns[KEY_SPACE]) {
         InputPoll();
+        if (g_window->windowClosed || g_input.keys[KEY_ESC])
+            return;
 
         BitmapClear(g_window->bmp, g_colourBlack);
 
@@ -29,6 +36,40 @@ void StretchBlitMain() {
         int x = g_window->bmp->width * (0.5f + 0.2 * sin(time * 1.5)) - targetHeight / 2.0f;
         int y = g_window->bmp->height * (0.5f + 0.2 * sin(time * 1.3)) - targetWidth / 2.0f;
         StretchBlit(g_window->bmp, x, y, targetWidth, targetHeight, dogBmp);
+
+        UpdateWin();
+        WaitVsync();
+    }
+
+    // Benchmark the stretch blit.
+    while (!g_window->windowClosed && !g_input.keys[KEY_ESC]) {
+        InputPoll();
+
+        BitmapClear(g_window->bmp, g_colourBlack);
+
+        double time1 = GetRealTime();
+
+        int targetWidth = dogBmp->width * 0.1;
+        int targetHeight = dogBmp->width * 0.1;
+        StretchBlit(g_window->bmp, 10, 15, targetWidth, targetHeight, dogBmp);
+
+        double time2 = GetRealTime();
+
+        targetWidth = dogBmp->width * 0.7;
+        targetHeight = dogBmp->width * 0.7;
+        StretchBlit(g_window->bmp, 100, 15, targetWidth, targetHeight, dogBmp);
+
+        double time3 = GetRealTime();
+
+        targetWidth = dogBmp->width * 2.0;
+        targetHeight = dogBmp->width * 2.0;
+        StretchBlit(g_window->bmp, 400, 15, targetWidth, targetHeight, dogBmp);
+
+        double time4 = GetRealTime();
+
+        DrawTextLeft(g_defaultFont, g_colourWhite, g_window->bmp, 10, 0, "%.2fms", (time2 - time1) * 1000.0f);
+        DrawTextLeft(g_defaultFont, g_colourWhite, g_window->bmp, 100, 0, "%.2fms", (time3 - time2) * 1000.0f);
+        DrawTextLeft(g_defaultFont, g_colourWhite, g_window->bmp, 400, 0, "%.2fms", (time4 - time3) * 1000.0f);
 
         UpdateWin();
         WaitVsync();
