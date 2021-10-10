760cfbf7c06a1aba1b5756142d2f9a3595bdb516
Andrew Bainbridge <abainbridge@users.noreply.github.com>
2021-10-10 15:13:48 +0100

    Made the StretchBlit example exercise the clipping.

--- examples/stretch_blit.cpp
+++ examples/stretch_blit.cpp
@@ -29,12 +29,12 @@ void StretchBlitMain() {
         BitmapClear(g_window->bmp, g_colourBlack);
 
         double time = GetRealTime();
-        float scale = 0.1f + fabs(sin(time * 1.9) * 1.2);
+        float scale = 0.2f + powf(fabs(sin(time * 1.9) * 1.2), 2.0);
         int targetWidth = dogBmp->width * scale;
         int targetHeight = dogBmp->width * scale;
 
-        int x = g_window->bmp->width * (0.5f + 0.2 * sin(time * 1.5)) - targetHeight / 2.0f;
-        int y = g_window->bmp->height * (0.5f + 0.2 * sin(time * 1.3)) - targetWidth / 2.0f;
+        int x = g_window->bmp->width * (0.5f + 0.7 * sin(time * 2.5)) - targetHeight / 2.0f;
+        int y = g_window->bmp->height * (0.5f + 0.7 * sin(time * 1.3)) - targetWidth / 2.0f;
         StretchBlit(g_window->bmp, x, y, targetWidth, targetHeight, dogBmp);
 
         UpdateWin();
