3e0d468f49f43d40672d4a97a19a047294f39676
Andrew Bainbridge <abainbridge@users.noreply.github.com>
2022-04-08 11:31:51 +0100

    X11: Removed df_message_dialog_x11.cpp because the code is now in df_message_dialog.cpp.

--- src/df_message_dialog_x11.cpp
+++
@@ -1,9 +0,0 @@
-#include "df_message_dialog.h"
-#include <stdio.h>
-
-
-int MessageDialog(char const *title, char const *message, MessageDialogType type)
-{
-    printf("%s\n", message);
-    return 0;
-}
