2f8a5f2967342d58c8302605820ee9abde05a374
Andrew Bainbridge <abainbridge@users.noreply.github.com>
2022-08-13 12:49:19 +0100

    asteroids.cpp - Removed the ObjectType enum and instead gave each object type its own struct. This means we can get rid of the confusing thing of the Object.age being used to store info about rendering the thrust on the ship.

--- examples/asteroids.cpp
+++ examples/asteroids.cpp
@@ -8,43 +8,37 @@
 
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof(*(a)))
 #define M_PI 3.1415926535
+double const BULLET_LIFE = 1.1;
 
 
 enum { SCREEN_WIDTH = 1024 };
 enum { SCREEN_HEIGHT = 768 };
 
-enum ObjectType {
-    UNDEFINED,
-    SHIP,
-    BULLET,
-    BIG_ASTEROID
-};
-
-
 struct Vec2 {
     double x, y;
 };
 
-struct Object {
-    ObjectType type;
+struct Ship {
     Vec2 pos;
     Vec2 vel;
     double angleRadians;
+    double thrustSeconds;
+};
+
+struct Bullet {
+    Vec2 pos;
+    Vec2 vel;
     double age;
 };
 
-
-Object g_ship = {
-    SHIP,
-    { SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 }, // x, y
-    { 0.0, 0.0 }, // vel
-    1.5708, // angleRadians
-    0.0 // age
+struct Asteroid {
+    Vec2 pos;
+    Vec2 vel;
 };
 
-Object g_bullets[4];
-
-Object g_asteroids[4];
+Ship g_ship;
+Bullet g_bullets[4];
+Asteroid g_asteroids[4];
 
 
 // ****************************************************************************
@@ -92,12 +86,11 @@ void RenderLinePath(DfBitmap *bmp, Vec2 posOffset, Vec2 *verts, int numVerts) {
 void FireBullet() {
     int i;
     for (i = 0; i < ARRAY_SIZE(g_bullets); i++)
-    if (g_bullets[i].type == UNDEFINED) break;
+    if (g_bullets[i].age > BULLET_LIFE) break;
 
     if (i == ARRAY_SIZE(g_bullets)) return;
 
     double const BULLET_SPEED = 430.0;
-    g_bullets[i].type = BULLET;
     g_bullets[i].pos.x = g_ship.pos.x;
     g_bullets[i].pos.y = g_ship.pos.y;
     g_bullets[i].vel.x = g_ship.vel.x + sin(g_ship.angleRadians) * BULLET_SPEED;
@@ -105,22 +98,20 @@ void FireBullet() {
     g_bullets[i].age = 0.0;
 }
 
-void AdvanceBullet(Object *obj, double advanceTime) {
-    obj->age += advanceTime;
-    double const BULLET_LIFE = 1.1;
-    if (obj->age > BULLET_LIFE) {
-        obj->type = UNDEFINED;
+void AdvanceBullet(Bullet *bul, double advanceTime) {
+    bul->age += advanceTime;
+    if (bul->age > BULLET_LIFE) {
         return;
     }
 
-    obj->pos.x += obj->vel.x * advanceTime;
-    obj->pos.y += obj->vel.y * advanceTime;
-    WrapPosition(&obj->pos);
+    bul->pos.x += bul->vel.x * advanceTime;
+    bul->pos.y += bul->vel.y * advanceTime;
+    WrapPosition(&bul->pos);
 }
 
-void RenderBullet(DfBitmap *bmp, Object *obj) {
-    if (obj->type != UNDEFINED)
-        PutPix(bmp, obj->pos.x, obj->pos.y, g_colourWhite);
+void RenderBullet(DfBitmap *bmp, Bullet *bul) {
+    if (bul->age < BULLET_LIFE)
+        PutPix(bmp, bul->pos.x, bul->pos.y, g_colourWhite);
 }
 
 
@@ -128,34 +119,42 @@ void RenderBullet(DfBitmap *bmp, Object *obj) {
 // Ship
 // ****************************************************************************
 
-void AdvanceShip(DfWindow *win, Object *obj) {
+void InitShip() {
+    g_ship.pos.x = SCREEN_WIDTH / 2;
+    g_ship.pos.y = SCREEN_HEIGHT / 2;
+    g_ship.vel.x = g_ship.vel.y = 0.0;
+    g_ship.angleRadians = 1.5708;
+    g_ship.thrustSeconds = 0.0;
+}
+
+void AdvanceShip(DfWindow *win) {
     if (win->input.keys[KEY_LEFT])
-        obj->angleRadians -= 6.3 * win->advanceTime;
+        g_ship.angleRadians -= 6.3 * win->advanceTime;
     if (win->input.keys[KEY_RIGHT])
-        obj->angleRadians += 6.3 * win->advanceTime;
+        g_ship.angleRadians += 6.3 * win->advanceTime;
     if (win->input.keys[KEY_UP]) {
         double const thrustForce = 5.5;
-        obj->vel.x += sin(obj->angleRadians) * thrustForce;
-        obj->vel.y -= cos(obj->angleRadians) * thrustForce;
+        g_ship.vel.x += sin(g_ship.angleRadians) * thrustForce;
+        g_ship.vel.y -= cos(g_ship.angleRadians) * thrustForce;
     }
     if (win->input.keyDowns[KEY_SPACE])
         FireBullet();
 
     double const frictionCoef = 0.21;
-    obj->vel.x -= obj->vel.x * win->advanceTime * frictionCoef;
-    obj->vel.y -= obj->vel.y * win->advanceTime * frictionCoef;
-    obj->pos.x += obj->vel.x * win->advanceTime;
-    obj->pos.y += obj->vel.y * win->advanceTime;
+    g_ship.vel.x -= g_ship.vel.x * win->advanceTime * frictionCoef;
+    g_ship.vel.y -= g_ship.vel.y * win->advanceTime * frictionCoef;
+    g_ship.pos.x += g_ship.vel.x * win->advanceTime;
+    g_ship.pos.y += g_ship.vel.y * win->advanceTime;
 
     if (win->input.keys[KEY_UP])
-        obj->age += win->advanceTime;
+        g_ship.thrustSeconds += win->advanceTime;
     else
-        obj->age = 0.0;
+        g_ship.thrustSeconds = 0.0;
 
-    WrapPosition(&obj->pos);
+    WrapPosition(&g_ship.pos);
 }
 
-void RenderShip(DfBitmap *bmp, Object *obj) {
+void RenderShip(DfBitmap *bmp) {
     enum { NUM_VERTS = 6 };
     Vec2 shipverts[NUM_VERTS] = { 
         { 0.0, -12.0 },
@@ -166,11 +165,11 @@ void RenderShip(DfBitmap *bmp, Object *obj) {
         { 0.0, -12.0 }
     };
 
-    RotateVerts(shipverts, NUM_VERTS, obj->angleRadians);
-    RenderLinePath(bmp, obj->pos, shipverts, NUM_VERTS);
+    RotateVerts(shipverts, NUM_VERTS, g_ship.angleRadians);
+    RenderLinePath(bmp, g_ship.pos, shipverts, NUM_VERTS);
 
     // Render rocket thrust.
-    int intAge = obj->age * 20.0;
+    int intAge = g_ship.thrustSeconds * 20.0;
     if (intAge & 1) {
         Vec2 rocketverts[] = {
             { 3.5, 8.0 },
@@ -178,8 +177,8 @@ void RenderShip(DfBitmap *bmp, Object *obj) {
             { -3.5, 8.0 }
         };
 
-        RotateVerts(rocketverts, ARRAY_SIZE(rocketverts), obj->angleRadians);
-        RenderLinePath(bmp, obj->pos, rocketverts, ARRAY_SIZE(rocketverts));
+        RotateVerts(rocketverts, ARRAY_SIZE(rocketverts), g_ship.angleRadians);
+        RenderLinePath(bmp, g_ship.pos, rocketverts, ARRAY_SIZE(rocketverts));
     }
 }
 
@@ -188,15 +187,15 @@ void RenderShip(DfBitmap *bmp, Object *obj) {
 // Asteroid
 // ****************************************************************************
 
-void AdvanceAsteroid(Object *obj, double advanceTime) {
-    obj->pos.x += obj->vel.x * advanceTime;
-    obj->pos.y += obj->vel.y * advanceTime;
-    WrapPosition(&obj->pos);
+void AdvanceAsteroid(Asteroid *ast, double advanceTime) {
+    ast->pos.x += ast->vel.x * advanceTime;
+    ast->pos.y += ast->vel.y * advanceTime;
+    WrapPosition(&ast->pos);
 }
 
-void RenderAsteroid(DfBitmap *bmp, Object *obj) {
+void RenderAsteroid(DfBitmap *bmp, Asteroid *ast) {
     Vec2 verts[12];
-    srand((unsigned)obj);
+    srand((unsigned)ast);
     double angle = 0.0;
     double angleIncrement = M_PI * 2.0 / (ARRAY_SIZE(verts) - 1);
     double avgRadius = 30.0;
@@ -210,8 +209,8 @@ void RenderAsteroid(DfBitmap *bmp, Object *obj) {
     verts[ARRAY_SIZE(verts) - 1].x = verts[0].x;
     verts[ARRAY_SIZE(verts) - 1].y = verts[0].y;
 
-    RotateVerts(verts, ARRAY_SIZE(verts), obj->angleRadians);
-    RenderLinePath(bmp, obj->pos, verts, ARRAY_SIZE(verts));
+    RotateVerts(verts, ARRAY_SIZE(verts), 0.0);
+    RenderLinePath(bmp, ast->pos, verts, ARRAY_SIZE(verts));
 }
 
 
@@ -223,26 +222,26 @@ void AsteroidsMain() {
     DfWindow *win = CreateWin(SCREEN_WIDTH, SCREEN_HEIGHT, WT_WINDOWED_RESIZEABLE, "Asteroids Example");
 
     g_defaultFont = LoadFontFromMemory(df_mono_7x13, sizeof(df_mono_7x13));
+
+    InitShip();
     for (int i = 0; i < ARRAY_SIZE(g_asteroids); i++) {
-        g_asteroids[i].type = BIG_ASTEROID;
         g_asteroids[i].pos.x = rand() % SCREEN_WIDTH;
         g_asteroids[i].pos.y = rand() % SCREEN_HEIGHT;
         g_asteroids[i].vel.x = rand() % 100;
         g_asteroids[i].vel.y = rand() % 100;
-        g_asteroids[i].angleRadians = 0.0;
     }
 
     while (!win->windowClosed && !win->input.keys[KEY_ESC]) {
         BitmapClear(win->bmp, g_colourBlack);
         InputPoll(win);
 
-        AdvanceShip(win, &g_ship);
+        AdvanceShip(win);
         for (int i = 0; i < ARRAY_SIZE(g_bullets); i++)
             AdvanceBullet(&g_bullets[i], win->advanceTime);
         for (int i = 0; i < ARRAY_SIZE(g_asteroids); i++)
             AdvanceAsteroid(&g_asteroids[i], win->advanceTime);
 
-        RenderShip(win->bmp, &g_ship);
+        RenderShip(win->bmp);
         for (int i = 0; i < ARRAY_SIZE(g_bullets); i++)
             RenderBullet(win->bmp, &g_bullets[i]);
         for (int i = 0; i < ARRAY_SIZE(g_asteroids); i++)
