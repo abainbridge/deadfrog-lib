5bce1b3c349f82c7ae511b27e62b5abde3c290de
Andrew Bainbridge <a@b>
2023-01-01 11:13:47 +0000

    Font tweaks - mainly make apostrophe be vertical not slanted.

--- src/fonts/df_prop.h
+++ src/fonts/df_prop.h
@@ -8,5 +8,5 @@ extern unsigned const df_prop_7x13[549];
 extern unsigned const df_prop_8x15[572];
 extern unsigned const df_prop_9x17[650];
 extern unsigned const df_prop_11x20[941];
-extern unsigned const df_prop_14x26[1311];
+extern unsigned const df_prop_14x26[1309];
 extern unsigned const df_prop_16x30[1442];
