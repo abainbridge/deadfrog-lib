0851fd5526fefd8a840bfaf2d933f892d09a3dce
Andrew Bainbridge <a@b>
2023-02-10 21:13:02 +0000

    Font tweaks, mainly to the £ glyphs.

--- src/fonts/df_prop.h
+++ src/fonts/df_prop.h
@@ -7,6 +7,6 @@ extern unsigned const df_prop_6x10[497];
 extern unsigned const df_prop_7x13[549];
 extern unsigned const df_prop_8x15[572];
 extern unsigned const df_prop_9x17[650];
-extern unsigned const df_prop_11x20[941];
+extern unsigned const df_prop_11x20[942];
 extern unsigned const df_prop_14x26[1309];
-extern unsigned const df_prop_16x30[1442];
+extern unsigned const df_prop_16x30[1441];
