6f4ed835faf3fcdbc0709fd9084af3bb3ff80056
Andrew Bainbridge <2005@deadfrog.co.uk>
2023-11-21 19:38:59 +0000

    Tweaked the shape of larger "A" glyphs.

--- src/fonts/df_mono.h
+++ src/fonts/df_mono.h
@@ -11,7 +11,7 @@ extern unsigned const df_mono_6x10[442];
 extern unsigned const df_mono_7x13[495];
 extern unsigned const df_mono_8x15[517];
 extern unsigned const df_mono_9x17[596];
-extern unsigned const df_mono_11x20[890];
+extern unsigned const df_mono_11x20[889];
 extern unsigned const df_mono_14x26[1257];
 extern unsigned const df_mono_16x30[1393];
 
