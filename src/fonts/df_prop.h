3befbd827c34143475de645ab1fe81666251b30a
Andrew Bainbridge <2005@deadfrog.co.uk>
2024-01-01 11:14:58 +0000

    Tweaked t and f glyphs. Lowered the cross bar on the f and moved the vertical bar right on both t and f. This seems to be more in keeping with what most other fonts look like.

--- src/fonts/df_prop.h
+++ src/fonts/df_prop.h
@@ -11,7 +11,7 @@ extern unsigned const df_prop_7x10[509];
 extern unsigned const df_prop_7x13[549];
 extern unsigned const df_prop_8x15[572];
 extern unsigned const df_prop_10x18[749];
-extern unsigned const df_prop_11x20[940];
+extern unsigned const df_prop_11x20[941];
 extern unsigned const df_prop_14x26[1309];
 extern unsigned const df_prop_16x30[1441];
 
