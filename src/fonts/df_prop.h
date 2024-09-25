caa4bfab681dbef4c35de4f7522e19ecb55f17ac
Andy B <a@b.com>
2024-09-25 21:47:22 +0100

    Improved the accented vowels in df_prop font.

--- src/fonts/df_prop.h
+++ src/fonts/df_prop.h
@@ -9,8 +9,8 @@ extern "C" {
 extern DfFontSource df_prop;
 extern unsigned const df_prop_7x10[509];
 extern unsigned const df_prop_7x13[549];
-extern unsigned const df_prop_8x15[572];
-extern unsigned const df_prop_10x18[749];
+extern unsigned const df_prop_8x15[573];
+extern unsigned const df_prop_10x18[774];
 extern unsigned const df_prop_11x20[941];
 extern unsigned const df_prop_14x26[1309];
 extern unsigned const df_prop_16x30[1441];
