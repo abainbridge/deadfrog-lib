22add7edf925bd9bc7f43952e3fbccdd7c5ede04
Andy B <a@b.com>
2024-10-29 09:03:25 +0000

    Made 'l' glyph in mono font look less like an 'L' in several sizes.

--- src/fonts/df_mono.h
+++ src/fonts/df_mono.h
@@ -9,7 +9,7 @@ extern "C" {
 extern DfFontSource df_mono;
 extern unsigned const df_mono_6x10[442];
 extern unsigned const df_mono_7x13[495];
-extern unsigned const df_mono_8x15[517];
+extern unsigned const df_mono_8x15[518];
 extern unsigned const df_mono_9x18[674];
 extern unsigned const df_mono_11x20[889];
 extern unsigned const df_mono_14x26[1257];
